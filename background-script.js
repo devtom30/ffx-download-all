100a37f8004e534792d387d14ba68e1346cd3359
devtom30 <devtom30@gmail.com>
2025-01-01 18:09:23 +0100

    background script open new tab

--- background-script.js
+++ background-script.js
@@ -7,13 +7,20 @@ browser.runtime.onInstalled.addListener(() => {
     console.log("Installed");
 });
 
+let onCreated = (tab) => {
+    console.log("created tab " + tab);
+}
+let onError = (error) => {
+    console.log("error on creating tab " + error);
+}
+
 let func = ((message, sender, sendResponse) => {
     if (message.action && message.action === "open-page") {
         console.log(message);
-        /*let creating = browser.tabs.create({
-            url: "https://example.org",
+        let creating = browser.tabs.create({
+            url: message.url,
         });
-        creating.then(onCreated, onError);*/
+        creating.then(onCreated, onError);
     }
 });
 browser.runtime.onMessage.addListener(func);
